ed5d4206d6eea9819609a8bd8e48613dda3f060a
groundbreakers <sudheermallela@gmail.com>
2014-08-03 19:52:09 +0100

    presentation controller

---
+++ TechTuesdayFeedbackTool/TechTuesdayFeedbackTool/TechTuesdayFeedbackTool/Controllers/PresentationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechTuesdayFeedbackTool.Domain;
+using TechTuesdayFeedbackTool.Repository;
+
+namespace TechTuesdayFeedbackTool.Controllers
+{
+    public class PresentationController : Controller
+    {
+        private IRepository<PresentationDetails> presentationsList;
+        //
+        // GET: /Presentation/
+        public ActionResult Index()
+        {
+            presentationsList = new Repository<PresentationDetails>();
+            return View(presentationsList.Query().ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PresentationDetails presentation)
+        {
+            presentationsList = new Repository<PresentationDetails>();
+            presentation.Date = DateTime.Now;
+            if(presentationsList.Save(presentation))
+            {
+                return RedirectToAction("Index", new { creationSuccess = "true" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "Creation failed!");
+            }
+            return View();
+        }
+	}
+}
